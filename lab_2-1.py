738e34afe03ae9e6fd1913bc2bd0df9af304083e
Sean O'Neil <p24soneil@fairfieldprep.org>
2023-09-21 12:01:28 -0400

    lab submission

--- lab_2-1.py
+++ lab_2-1.py
@@ -1,9 +1,17 @@
 #Create a python file named lab_2-1.py
 #Input the following into the Python interpreter and see what the output is. Be mindful of spaces. Each expression needs to be visible in your final code.
-"""
+""""
 123 + 321 - 321
 20+ 202020 - 420 + 20
 2 * 4 * 6 * 8		9 / 5		9 // 5
 13/0	3 % 10	3 % 10
 2 ** 3		2 * 2 * 2		0/0
 """
+a = 123 + 312 - 321
+b = 20 + 202020 - 420 + 20
+c = 2 * 4 * 6 * 8
+d = 9 / 5   9 // 5
+e = 13/0 3 % 10 3 % 10
+f = 2 ** 3 2 * 2 * 2 
+g = 0/0 
+print (a, b, c, d, e, f, g, )
--- lab_2-2.py
+++ lab_2-2.py
@@ -1,13 +1,15 @@
 # 1) Create a Python file named lab_2-2.py
 
 # 2) Set a equal to 3 and b equal to a
-
+a = 3
+b = a
 # 3) Print the value of b and the value of a.
-
+print (a, b)
 # 4) What is the value of each variable and why? (Answer as a comment)
-
+# It showed two 3's
 # 5) Set a equal to 7.
-
+a = 7
 # 6) Print the value of a and the value of b
-
+print (a, b)
 # 7) What is the value of each variable and why? (Answer as a comment)
+# a's value switched to 7, but b stayed as 3
